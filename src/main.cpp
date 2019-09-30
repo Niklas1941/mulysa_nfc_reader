74df5fa7bfb12ce37b261b88a2e99b7ca90999f8
Tatu Wikman <tatu.wikman@gmail.com>
2019-09-30 22:55:28 +0300

    reading works

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,37 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include <SPI.h>
-#include <Adafruit_PN532.h>
+#include <PN532_SPI.h>
+#include "PN532.h"
 
-#define PN532_IRQ (2)
-#define PN532_RESET (3)
-Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+#define SPI_SS_PIN (5)
+
+PN532_SPI pn532spi(SPI, SPI_SS_PIN);
+PN532 nfc(pn532spi);
 
 void setup()
 {
-
   Serial.begin(115200);
 
-  // init nfc
+  Serial.println("Hello!");
+
   nfc.begin();
+
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (!versiondata)
   {
-    Serial.print("Found chip PN5");
-    Serial.println((versiondata >> 24) & 0xFF, HEX);
-    Serial.print("Firmware ver. ");
-    Serial.print((versiondata >> 16) & 0xFF, DEC);
-    Serial.print('.');
-    Serial.println((versiondata >> 8) & 0xFF, DEC);
+    Serial.print("Didn't find PN53x board");
+    while (1) {
+      delay(10);
+    }
   }
-  else
-  {
-    Serial.println("could not initialize nfc reader");
-  }
+
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5");
+  Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver. ");
+  Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.');
+  Serial.println((versiondata >> 8) & 0xFF, DEC);
 
   // Set the max number of retry attempts to read from a card
   // This prevents us from waiting forever for a card, which is
@@ -36,36 +40,30 @@ void setup()
 
   // configure board to read RFID tags
   nfc.SAMConfig();
-
-  Serial.println("Now read your card");
 }
 
-void loop()
-{
+void loop(void) {
   boolean success;
-  uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0}; // Buffer to store the returned UID
-  uint8_t uidLength;                     // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
   // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
   // 'uid' will be populated with the UID, and uidLength will indicate
   // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
 
-  if (success)
-  {
+  if (success) {
     Serial.println("Found a card!");
-    Serial.print("UID Length: ");
-    Serial.print(uidLength, DEC);
-    Serial.println(" bytes");
+    Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
     Serial.print("UID Value: ");
-    for (uint8_t i = 0; i < uidLength; i++)
+    for (uint8_t i=0; i < uidLength; i++)
     {
-      Serial.print(" 0x");
-      Serial.print(uid[i], HEX);
+      Serial.print(" 0x");Serial.print(uid[i], HEX);
     }
     Serial.println("");
-    // Wait 1 second before continuing
-    delay(1000);
+
+    // wait until the card is taken away
+    while (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength)) {}
   }
   else
   {
